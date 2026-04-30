cef273bc3b47eb3ba9718bd5000bf448936d73e7
Chris Lauretano <1905121+lauretano@users.noreply.github.com>
2026-04-30 11:44:56 -0400

    fix: more quickshell package compat troubleshooting

--- build_files/apps-kansei.sh
+++ build_files/apps-kansei.sh
@@ -86,16 +86,16 @@ dnf5 -y install --skip-unavailable river xdg-desktop-portal-wlr xdg-desktop-port
 dnf5 -y copr enable errornointernet/quickshell
 dnf5 -y copr enable avengemedia/dms
 dnf5 -y copr enable zhangyi6324/noctalia-shell
-dnf5 -y copr enable brycensranch/gpu-screen-recorder-git
+#dnf5 -y copr enable brycensranch/gpu-screen-recorder-git
 
-dnf5 -y install noctalia-qs
-dnf5 -y install --skip-unavailable gpu-screen-recorder-ui gpu-screen-recorder-gtk gpu-screen-recorder-notification cava qt6-qtmultimedia i2c-tools wl-clipboard cliphist matugen dgop
+#dnf5 -y install noctalia-qs
+dnf5 -y install --skip-unavailable cava qt6-qtmultimedia i2c-tools wl-clipboard cliphist matugen dgop
 dnf5 -y install dms noctalia-shell
 
 dnf5 copr disable errornointernet/quickshell
 dnf5 copr disable avengemedia/dms
 dnf5 copr disable zhangyi6324/noctalia-shell
-dnf5 copr disable brycensranch/gpu-screen-recorder-git
+#dnf5 copr disable brycensranch/gpu-screen-recorder-git
 
 dnf5 -y copr enable ublue-os/packages
 dnf5 -y config-manager setopt copr:copr.fedorainfracloud.org:ublue-os:packages.enabled=0
